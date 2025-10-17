192153ec3acc5565bf6e8f1a2eed99501bab7995
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-10-17 13:57:15 +0900

    Mysqlの設定をcomposeから消した

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db18b1b73bcfcbae0cb7d98b946654ad5fc35756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa07fd0c8a98ce45d1eb38efd124d18d0bd01bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
