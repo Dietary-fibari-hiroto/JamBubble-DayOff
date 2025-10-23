9b424dd0a4bb3f5556fbe54c39122393b644aecc
Irbea192 <elepop777@gmail.com>
2025-10-23 17:02:55 +0900

    User.csのデータ注釈をコメントアウト

--- Server/Server/Models/User.cs
+++ Server/Server/Models/User.cs
@@ -1,16 +1,16 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Server.Models
 {
-    [Table("Users")]
+    //[Table("Users")]
     public class Users
     {
-        [Key]
+        //[Key]
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; }
+        //[Required]
+        //[MaxLength(100)]
+        public required string Name { get; set; }
     }
 }
