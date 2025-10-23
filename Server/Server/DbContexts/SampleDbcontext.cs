1ee55b50d1d762d1846758aeb6a1ee26c343fc18
Irbea192 <elepop777@gmail.com>
2025-10-23 17:02:43 +0900

    SampleDbcontextクラスを新規作成し、Users DbSetを追加

---
+++ Server/Server/DbContexts/SampleDbcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.DbContexts
+{
+    public class SampleDbcontext : DbContext
+    {
+        public SampleDbcontext(DbContextOptions<SampleDbcontext> options) : base(options) { }
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
