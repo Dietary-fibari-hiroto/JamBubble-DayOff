24bbafc200433e6d0667485c4496d24e1d8a3b2c
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-10-29 15:57:11 +0900

    WorkLocationプロジェクト作成

--- Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adce000932fae8c030411cc4ab5a8f5376a890a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739b3ee2a1c8dcbc23bd1b2ddee40d3825f4390c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
