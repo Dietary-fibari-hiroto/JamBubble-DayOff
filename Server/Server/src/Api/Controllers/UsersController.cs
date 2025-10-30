098ba7344a9911b3ba73dd4959f2a30fea9da338
Irbea192 <elepop777@gmail.com>
2025-10-31 00:55:45 +0900

    ユーザーコントローラーに認証機能を追加し、AuthControllerを実装

--- Server/Server/src/Api/Controllers/UsersController.cs
+++ Server/Server/src/Api/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
 using Server.src.Interfaces;
 
 namespace Server.src.Api.Controllers
@@ -20,10 +22,37 @@ namespace Server.src.Api.Controllers
         public UserController(IUserService userService) {
             _userService = userService;
         }
+
         [HttpGet]
-        public async Task<IActionResult> GetAll() =>Ok( await _userService.GetAllUserAsync());
-        
+        public async Task<IActionResult> GetAll() => Ok(await _userService.GetAllUserAsync());
+
+        //[Authorize]
+        //[HttpGet]
+
+
+
     }
 
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var token = await _authService.LoginAsync(request.Email, request.Password);
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { token });
+        }
+
+    }
 
 }
