03a3e5ecfdfe573ba8dbb948226ff66e50f7c116
Irbea192 <elepop777@gmail.com>
2025-10-31 00:58:08 +0900

    Emailでユーザーを取得するメソッドを追加

--- Server/Server/src/Infrastructure/Repositories/UserRepository.cs
+++ Server/Server/src/Infrastructure/Repositories/UserRepository.cs
@@ -13,8 +13,10 @@ namespace Server.src.Repositories
         }
 
         public async Task<IEnumerable<User>> GetAllAsync() => await _context.Users.ToListAsync();
+        // IDでユーザーを取得
         public async Task<User?> GetByIdAsync(int id) => await _context.Users.FindAsync(id);
-        //public async Task<User?> AddAsync(User user) => await _context.Users.AddAsync(user);
+        // Emailでユーザーを取得
+        public async Task<User?> GetByEmailAsync(string email) => await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         public async Task<User?> AddAsync(User user)
         {
             var entry = await _context.Users.AddAsync(user);
