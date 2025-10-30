80608ba09ccfe296258027e2ab33b2ac032dc22e
Irbea192 <elepop777@gmail.com>
2025-10-31 00:56:41 +0900

    IAuthServiceインターフェースを追加

---
+++ Server/Server/src/Core/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Server.src.Entities;
+
+namespace Server.src.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string?> LoginAsync(string email, string password);
+        //bool VerifyPassword(User user, string password);
+    }
+}
