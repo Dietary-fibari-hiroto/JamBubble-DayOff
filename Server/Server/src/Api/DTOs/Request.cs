d9259da05812bb06488d773c451668fa38ccb543
Irbea192 <elepop777@gmail.com>
2025-11-02 20:36:33 +0900

    ユーザー登録およびログインのリクエストを定義

---
+++ Server/Server/src/Api/DTOs/Request.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Server.src.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class RegisterUserRequest
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public int Gender { get; set; } = 0;
+        [Required]
+        public DateOnly Birthday { get; set; }
+        public bool? IsStreetPass { get; set; } = false;
+        public string? ImgUrl { get; set; } = null;
+    }
+}
---
+++ Server/Server/src/Api/DTOs/RequestFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Server.src.Entities;
+
+namespace Server.src.DTOs
+{
+    public class RequestFilter : ISchemaFilter
+    {
+        void ISchemaFilter.Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.Type == typeof(User))
+            {
+                schema.Example = new OpenApiObject
+                {
+                    ["name"] = new OpenApiString("test"),
+                    ["birthday"] = new OpenApiString("2025-11-01"),
+                    ["email"] = new OpenApiString("test@test.com"),
+                    ["password"] = new OpenApiString("password"),
+                    ["gender"] = new OpenApiInteger(0),
+                    ["isStreetPass"] = new OpenApiBoolean(false),
+                    ["imgUrl"] = new OpenApiString("")
+                };
+            }
+
+            if (context.Type == typeof(LoginRequest))
+            {
+                schema.Example = new OpenApiObject
+                {
+                    ["Email"] = new OpenApiString("test@test.com"),
+                    ["Password"] = new OpenApiString("password")
+                };
+            }
+        }
+    }
+}
