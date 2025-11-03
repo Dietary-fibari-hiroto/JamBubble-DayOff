48b6120b123fe9a158f7ef2de3222b3c9665e153
Irbea192 <elepop777@gmail.com>
2025-11-03 16:44:41 +0900

    ユーザーにメッセージを追加したマイグレーション

---
+++ Server/Server/Migrations/20251103045035_AddUserMessage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "message",
+                table: "users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "message",
+                table: "users");
+        }
+    }
+}
