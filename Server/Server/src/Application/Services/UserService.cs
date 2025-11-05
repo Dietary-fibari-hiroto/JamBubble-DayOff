a6e3c04b18a79fdeea6ee631e5b396163689a425
Irbea192 <elepop777@gmail.com>
2025-11-05 11:00:26 +0900

    メソッドシグネチャを更新 Email重複チェックを追加

--- Server/Server/src/Application/Services/UserService.cs
+++ Server/Server/src/Application/Services/UserService.cs
@@ -24,37 +24,61 @@ namespace Server.src.Services
         {
             // IDで取得
             var user = await _repo.GetByIdAsync(userId);
-            if (user == null) return null;
+            if (user == null)
+            {
+                return null;
+            }
 
             var userResponseDto = new UserAllDataResponseDto(user);
 
             return userResponseDto;
         }
-        public async Task<UserResponseDto?> AddUserAsync(User user)
+        public async Task<UserResponseDto?> AddUserAsync(RegisterUserRequestDto userDto)
         {
             // すでに同じEmailのユーザーが存在するか確認
-            var existingUser = await _repo.GetByEmailAsync(user.Email);
-            if (existingUser != null) return null; 
+            var existingUser = await _repo.GetByEmailAsync(userDto.Email);
+            if (existingUser != null)
+            {
+                throw new InvalidOperationException("EmailConflict");
+            }
+
+            var user = userDto.RequestToUser(new User());
 
             // パスワードのハッシュ化
             user.Password = _passwordHasher.HashPassword(user, user.Password);
 
             var addedUser = await _repo.AddAsync(user);
-            if (addedUser == null) return null;
 
-            var userResponseDto = new UserResponseDto(addedUser);
-            return userResponseDto;
+            return new UserResponseDto(addedUser);
         }
 
-        public async Task<User?> UpdateUserAsync(User updateData, int userId)
+        public async Task<UserAllDataResponseDto?> UpdateUserAsync(UpdateUserAllDataRequestDto updateDataDto, int userId)
         {
             // IDで取得
             var updateUser = await _repo.GetByIdAsync(userId);
             if (updateUser == null) return null;
 
+            // Emailが重複していないかをチェック
+            var newEmail = updateDataDto.userDto?.Email;
+            if(newEmail != null && newEmail != updateUser.Email)
+            {
+                var existingUser = await _repo.GetByEmailAsync(newEmail);
+                if (existingUser != null && existingUser.Id != userId)
+                {
+                    throw new InvalidOperationException("EmailConflict");
+                }
+            }
+            string? newPassword = updateDataDto.userDto?.Password; // 新しい平文のパスワード一時保存
+            updateUser = updateDataDto.RequestDtoToEntitie(updateUser);
+            if (!string.IsNullOrEmpty(newPassword))
+            {
+                // パスワードのハッシュ化
+                updateUser.Password = _passwordHasher.HashPassword(updateUser, newPassword);
+            }
+            
+            await _repo.UpdateAsync(updateUser); // 更新処理
 
-
-            return updateUser;
+            return new UserAllDataResponseDto(updateUser);
         }
 
         public async Task<bool> DeleteUserAsync(int userId)
--- Server/Server/src/Core/Interfaces/Services/IUserService.cs
+++ Server/Server/src/Core/Interfaces/Services/IUserService.cs
@@ -6,8 +6,8 @@ namespace Server.src.Interfaces
     public interface IUserService
     {
         public Task<UserAllDataResponseDto?> GetUserAllDataAsync(int userId);
-        public Task<UserResponseDto?> AddUserAsync(User user);
-        public Task<User?> UpdateUserAsync(User updateData, int userId);
+        public Task<UserResponseDto?> AddUserAsync(RegisterUserRequestDto user);
+        public Task<UserAllDataResponseDto?> UpdateUserAsync(UpdateUserAllDataRequestDto updateData, int userId);
         public Task<bool> DeleteUserAsync(int userId);
     }
 }
