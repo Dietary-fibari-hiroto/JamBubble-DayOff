73b8953b7cdbd7a2db1f6b381e86e321d2c440af
Irbea192 <elepop777@gmail.com>
2025-11-05 11:01:03 +0900

    ユーザーIDを取得するクラス追加

---
+++ Server/Server/src/Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+
+namespace Server.src.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int? GetUserId(this ClaimsPrincipal user)
+        {
+            if (user == null)
+            {
+                return null;
+            }
+
+            // JWTからユーザーIDを取得
+            var userIdString = user.Claims.FirstOrDefault(
+                c => c.Type == ClaimTypes.NameIdentifier
+                )?.Value;
+
+            if (string.IsNullOrEmpty(userIdString))
+            {
+                return null;
+            }
+
+            // Intに変換
+            if (!int.TryParse(userIdString, out var userId))
+            {
+                return null;
+            }
+
+            return userId;
+        }   
+    }
+}
