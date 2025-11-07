372f7e4259c05d766bb2cba1866a2a9f66b8434d
parkgy <every878@gmail.com>
2025-11-07 16:38:21 +0900

    sessionlinkとメニューバーを完成しました（ダイアログ含み）

--- Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739b3ee2a1c8dcbc23bd1b2ddee40d3825f4390c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab9e1d35899f7ffe00a242d6ac9d7ab51d9e320")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
