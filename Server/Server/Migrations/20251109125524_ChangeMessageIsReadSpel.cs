28ea9339be42ad91a8206253e33a757f0589fbd4
Irbea192 <elepop777@gmail.com>
2025-11-09 21:57:54 +0900

    誤字修正

---
+++ Server/Server/Migrations/20251109125524_ChangeMessageIsReadSpel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMessageIsReadSpel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_reqd",
+                table: "messages",
+                newName: "is_read");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_read",
+                table: "messages",
+                newName: "is_reqd");
+        }
+    }
+}
