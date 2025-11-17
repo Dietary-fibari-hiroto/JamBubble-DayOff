2aa57760a8da6dbcb18fd3989c2eb487a45992a4
MSI\2230165 <2230165@ecc.ac.jp>
2025-11-17 14:45:20 +0900

    ishizuka-sessionplaylistDialogのファイル

--- Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87e882654e02a5d41e8260f0de757670a2fa2fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8626a5c77d1a2c6658769c525c6cb0887feed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
