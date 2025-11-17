335386de2fd3c933d5fbb57ddc74ffd8a1559f9c
Irbea192 <elepop777@gmail.com>
2025-11-17 13:38:34 +0900

    呼び出しをコメントアウト

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -73,7 +73,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    await DevelopmentDataSeeder.SeedAsync(app.Services);
+    // await DevelopmentDataSeeder.SeedAsync(app.Services); // 開発用データ
 }
 
 app.UseHttpsRedirection(); //Http�Ń��N�G�X�g���ꂽ�Ƃ���Https�փ��_�C���N�g
